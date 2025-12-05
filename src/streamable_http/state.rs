febde19a586629da5ab59626867ecd56e645e8e7
Joseph Wortmann <joseph.wortmann@gmail.com>
2025-12-05 16:06:06 -0600

    Extract create_documentation to module level

--- src/streamable_http/state.rs
+++ src/streamable_http/state.rs
@@ -4,6 +4,45 @@ use rmcp::model::ClientInfo;
 use rmcp::service::{RoleClient, RoleServer, RunningService, Service, serve_client, serve_server};
 use tokio::io::duplex;
 
+async fn create_documentation(config: &Config) -> Result<String> {
+    async fn documentation_pair<S, C>(
+        service: S,
+        client: C,
+    ) -> (RunningService<RoleServer, S>, RunningService<RoleClient, C>)
+    where
+        S: Service<RoleServer>,
+        C: Service<RoleClient>,
+    {
+        let (srv_io, cli_io) = duplex(64 * 1024);
+        tokio::try_join!(
+            async {
+                serve_server(service, srv_io)
+                    .await
+                    .map_err(anyhow::Error::from)
+            },
+            async {
+                serve_client(client, cli_io)
+                    .await
+                    .map_err(anyhow::Error::from)
+            }
+        )
+        .expect("Failed to create documentation pair")
+    }
+
+    let (server, client) = documentation_pair(
+        crate::service::PluginService::new(&config).await?,
+        ClientInfo::default(),
+    )
+    .await;
+
+    let docs = server.service().generate_docs().await?;
+
+    server.cancel().await?;
+    client.cancel().await?;
+
+    Ok(docs)
+}
+
 #[derive(Clone)]
 pub struct ServerState {
     pub config: Config,
@@ -11,49 +50,10 @@ pub struct ServerState {
 }
 
 impl ServerState {
-    async fn create_documentation(config: &Config) -> Result<String> {
-        async fn documentation_pair<S, C>(
-            service: S,
-            client: C,
-        ) -> (RunningService<RoleServer, S>, RunningService<RoleClient, C>)
-        where
-            S: Service<RoleServer>,
-            C: Service<RoleClient>,
-        {
-            let (srv_io, cli_io) = duplex(64 * 1024);
-            tokio::try_join!(
-                async {
-                    serve_server(service, srv_io)
-                        .await
-                        .map_err(anyhow::Error::from)
-                },
-                async {
-                    serve_client(client, cli_io)
-                        .await
-                        .map_err(anyhow::Error::from)
-                }
-            )
-            .expect("Failed to create documentation pair")
-        }
-
-        let (server, client) = documentation_pair(
-            crate::service::PluginService::new(&config).await?,
-            ClientInfo::default(),
-        )
-        .await;
-
-        let docs = server.service().generate_docs().await?;
-
-        server.cancel().await?;
-        client.cancel().await?;
-
-        Ok(docs)
-    }
-
     pub async fn new(config: &Config) -> Result<Self> {
         Ok(Self {
             config: config.clone(),
-            documentation: Self::create_documentation(config).await?,
+            documentation: create_documentation(config).await?,
         })
     }
 }
