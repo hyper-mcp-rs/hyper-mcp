047ef4939c1940b57b73b7361bd1c30e366beaea
Joseph Wortmann <joseph.wortmann@gmail.com>
2025-12-11 10:09:25 -0600

    Move documentation generation inline into ServerState::new

--- src/streamable_http/state.rs
+++ src/streamable_http/state.rs
@@ -78,45 +78,6 @@ impl DiscoveryMetadata {
     }
 }
 
-async fn create_documentation(config: &Config) -> Result<String> {
-    async fn documentation_pair<S, C>(
-        service: S,
-        client: C,
-    ) -> (RunningService<RoleServer, S>, RunningService<RoleClient, C>)
-    where
-        S: Service<RoleServer>,
-        C: Service<RoleClient>,
-    {
-        let (srv_io, cli_io) = duplex(64 * 1024);
-        tokio::try_join!(
-            async {
-                serve_server(service, srv_io)
-                    .await
-                    .map_err(anyhow::Error::from)
-            },
-            async {
-                serve_client(client, cli_io)
-                    .await
-                    .map_err(anyhow::Error::from)
-            }
-        )
-        .expect("Failed to create documentation pair")
-    }
-
-    let (server, client) = documentation_pair(
-        crate::service::PluginService::new(&config).await?,
-        ClientInfo::default(),
-    )
-    .await;
-
-    let docs = server.service().generate_docs().await?;
-
-    server.cancel().await?;
-    client.cancel().await?;
-
-    Ok(docs)
-}
-
 async fn create_jwks(
     config: &Config,
 ) -> Result<(Vec<AuthorizationServerUrl>, HashMap<String, JwkSet>)> {
@@ -190,10 +151,46 @@ impl ServerState {
                 );
             }
         }
+
+        async fn temp_pair<S, C>(
+            service: S,
+            client: C,
+        ) -> (RunningService<RoleServer, S>, RunningService<RoleClient, C>)
+        where
+            S: Service<RoleServer>,
+            C: Service<RoleClient>,
+        {
+            let (srv_io, cli_io) = duplex(64 * 1024);
+            tokio::try_join!(
+                async {
+                    serve_server(service, srv_io)
+                        .await
+                        .map_err(anyhow::Error::from)
+                },
+                async {
+                    serve_client(client, cli_io)
+                        .await
+                        .map_err(anyhow::Error::from)
+                }
+            )
+            .expect("Failed to create documentation pair")
+        }
+
+        let (server, client) = temp_pair(
+            crate::service::PluginService::new(&config).await?,
+            ClientInfo::default(),
+        )
+        .await;
+
+        let docs = server.service().generate_docs().await?;
+
+        server.cancel().await?;
+        client.cancel().await?;
+
         Ok(Self {
             auth_servers,
             config: config.clone(),
-            docs: create_documentation(config).await?,
+            docs: docs,
             jwks: jwks,
         })
     }
