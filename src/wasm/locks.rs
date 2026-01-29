0eab196e38a76c3e73bf0e5da348ebc351a6b3b7
Joseph Wortmann <joseph.wortmann@gmail.com>
2026-01-29 17:40:40 -0600

    Switch to std::sync::LazyLock for DOWNLOAD_LOCKS

--- src/wasm/locks.rs
+++ src/wasm/locks.rs
@@ -1,10 +1,9 @@
 use dashmap::DashMap;
-use once_cell::sync::Lazy;
-use std::sync::Arc;
+use std::sync::{Arc, LazyLock};
 use tokio::sync::{Mutex, OwnedMutexGuard};
 use url::Url;
 
-pub static DOWNLOAD_LOCKS: Lazy<Locks> = Lazy::new(|| Locks(DashMap::new()));
+pub static DOWNLOAD_LOCKS: LazyLock<Locks> = LazyLock::new(|| Locks(DashMap::new()));
 
 pub struct Locks(DashMap<Url, Arc<Mutex<()>>>);
 
