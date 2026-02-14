6ec65a2b0905ed195c0b30cbe8b1eeb47926b19f
Joseph Wortmann <joseph.wortmann@gmail.com>
2026-02-14 16:09:10 -0600

    Use test writer in tests and stderr elsewhere

--- src/logging.rs
+++ src/logging.rs
@@ -6,14 +6,20 @@ static LOGGING: OnceCell<()> = OnceCell::new();
 #[ctor::ctor]
 fn _install_global_tracing() {
     LOGGING.get_or_init(|| {
-        tracing_subscriber::fmt()
+        let fmt = tracing_subscriber::fmt()
             .with_env_filter(
                 EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info")),
             )
-            .with_test_writer()
             .with_target(true)
             .with_line_number(true)
-            .with_ansi(false)
-            .init();
+            .with_ansi(false);
+
+        #[cfg(test)]
+        let fmt = fmt.with_test_writer();
+
+        #[cfg(not(test))]
+        let fmt = fmt.with_writer(std::io::stderr);
+
+        fmt.init();
     });
 }
